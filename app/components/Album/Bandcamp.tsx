e540c4054dd5c7e5bc872ed91a6a4d1cd312bd7e
Eli Gundry <eligundry@gmail.com>
2022-05-16 19:19:45 -0400

    Show just the album art for bandcamp albums

--- app/components/Album/Bandcamp.tsx
+++ app/components/Album/Bandcamp.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx'
 
 import { Heading, Container } from '~/components/Base'
 import ReviewButtons from './ReviewButtons'
+import useIsMobile from '~/hooks/useIsMobile'
 
 interface Props {
   albumID: string | number
@@ -19,14 +20,32 @@ const BandcampAlbum: React.FC<Props> = ({
   url,
   footer,
 }) => {
+  const isMobile = useIsMobile()
+  const params = [
+    `album=${albumID}`,
+    'size=large',
+    'bgcol=ffffff',
+    'linkcol=0687f5',
+    'tracklist=false',
+    'transparent=true',
+  ]
+
+  if (isMobile) {
+    params.push('minimal=true')
+  }
+
   return (
     <Container center>
       <Heading level="h2" className={clsx('mb-4')}>
         Have you heard <em>{album}</em> by {artist}?
       </Heading>
       <iframe
-        style={{ border: 0, width: '350px', height: '470px' }}
-        src={`https://bandcamp.com/EmbeddedPlayer/album=${albumID}/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/`}
+        style={{
+          border: 0,
+          width: '350px',
+          height: isMobile ? '350px' : '470px',
+        }}
+        src={`https://bandcamp.com/EmbeddedPlayer/${params.join('/')}`}
         seamless
         className={clsx('mx-auto')}
       >
