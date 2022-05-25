77de19550f379ab011c32df2ab5f1d379c14bc31
Eli Gundry <eligundry@gmail.com>
2022-05-25 18:02:16 -0400

    Reorder the home page and hide curated artists section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -56,6 +56,39 @@ export default function Index() {
           Don't know what to listen to?
           <br /> Let us recommend an album.
         </Heading>
+        <div className="artist">
+          <Heading level="h3" className={clsx('mb-2')}>
+            Artist
+          </Heading>
+          <Typography variant="hint" className={clsx('mb-2')}>
+            Want an album from an artist similar to one you like? What's their
+            name?
+          </Typography>
+          <RelatedArtistSearchForm />
+        </div>
+        <div className="genre">
+          <Heading level="h3" className={clsx('mb-2')}>
+            <Link to="/genre">Genre</Link>
+          </Heading>
+          <Typography variant="hint" className={clsx('mb-2')}>
+            Have a genre in mind? Search for it and we'll find you something.
+          </Typography>
+          <GenreSearchForm defaultGenres={data.topGenres} />
+        </div>
+        <div className="publications">
+          <Heading level="h3" className={clsx('mb-2')}>
+            Publications
+          </Heading>
+          <Typography variant="hint" className={clsx('mb-2')}>
+            Play a random album recommended by the pros.
+          </Typography>
+          <ButtonLinkGroup
+            items={data.publications}
+            toFunction={(publication) => `/publication/${publication.slug}`}
+            keyFunction={(publication) => publication.slug}
+            childFunction={(publication) => publication.name}
+          />
+        </div>
         <div className="spotify">
           <Heading level="h3" className={clsx('mb-2')}>
             Spotify
@@ -83,39 +116,6 @@ export default function Index() {
             New Release
           </ButtonLink>
         </div>
-        <div className="publications">
-          <Heading level="h3" className={clsx('mb-2')}>
-            Publications
-          </Heading>
-          <Typography variant="hint" className={clsx('mb-2')}>
-            Play a random album recommended by the pros.
-          </Typography>
-          <ButtonLinkGroup
-            items={data.publications}
-            toFunction={(publication) => `/publication/${publication.slug}`}
-            keyFunction={(publication) => publication.slug}
-            childFunction={(publication) => publication.name}
-          />
-        </div>
-        <div className="artist">
-          <Heading level="h3" className={clsx('mb-2')}>
-            Artist
-          </Heading>
-          <Typography variant="hint" className={clsx('mb-2')}>
-            Want an album from an artist similar to one you like? What's their
-            name?
-          </Typography>
-          <RelatedArtistSearchForm />
-        </div>
-        <div className="genre">
-          <Heading level="h3" className={clsx('mb-2')}>
-            <Link to="/genre">Genre</Link>
-          </Heading>
-          <Typography variant="hint" className={clsx('mb-2')}>
-            Have a genre in mind? Search for it and we'll find you something.
-          </Typography>
-          <GenreSearchForm defaultGenres={data.topGenres} />
-        </div>
         <div className="labels">
           <Heading level="h3" className={clsx('mb-2')}>
             <Link to="/label">Labels</Link>
@@ -125,20 +125,6 @@ export default function Index() {
             link above has some ones to check out.
           </Typography>
           <LabelSearchForm />
-        </div>
-        <div className="artists-and-groups">
-          <Heading level="h3" className={clsx('mb-2')}>
-            Curated Artists & Groups
-          </Heading>
-          <Typography variant="hint" className={clsx('mb-2')}>
-            Here are some artists and groups that I dig, maybe you'll like them?
-          </Typography>
-          <ButtonLinkGroup
-            items={data.artistGroupings}
-            toFunction={({ slug }) => `/group/${slug}`}
-            keyFunction={({ slug }) => slug}
-            childFunction={({ name }) => name}
-          />
         </div>
       </Container>
     </Layout>
