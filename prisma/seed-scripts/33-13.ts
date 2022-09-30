0755b344999d7f595bb55853b6d94c8824e97cd4
Eli Gundry <eligundry@gmail.com>
2022-09-30 02:06:50 -0400

    Convert the 33⅓ scraper to use bloomsbury.com
    This is a lot more accurate and easier to parse. Also, it allowed me to
    get the international editions.

--- prisma/seed-scripts/33-13.ts
+++ prisma/seed-scripts/33-13.ts
@@ -4,7 +4,7 @@ import { PrismaClient, Publication } from '@prisma/client'
 const prisma = new PrismaClient()
 
 // Pulls albums from the US version of 33 1/3.
-const US = async () => {
+const bloomsburyScraper = async (storeSlug: string) => {
   const publication = (await prisma.publication
     .create({
       data: {
@@ -29,7 +29,9 @@ const US = async () => {
   const page = await context.newPage()
 
   try {
-    const response = await page.goto('https://333sound.com/33-13-series/', {
+    const storeBaseURL = `https://www.bloomsbury.com/us/series/${storeSlug}`
+    console.log(`fetching ${storeBaseURL}`)
+    const response = await page.goto(storeBaseURL, {
       waitUntil: 'domcontentloaded',
     })
 
@@ -38,41 +40,61 @@ const US = async () => {
       return
     }
 
-    const rows = page.locator('.entry-content table tr td:nth-child(2)')
-    const count = await rows.count()
+    let shouldContinue = true
+    let pageIdx = 1
 
-    for (let i = 0; i < count; i++) {
-      const row = rows.nth(i)
-      const innerText = await row.innerText()
-      const firstLine = innerText.split('\n')[0]
-      const matches = innerText.match(/^\d+.[  ](.*)’s?[  ](.*)/)
-      const artist = matches?.[1]
-      const album = matches?.[2]
-      const url = await row
-        .locator('a[href*="bloomsbury.com"]:first-of-type')
-        .getAttribute('href')
-      console.log(firstLine, { artist, album, url })
+    do {
+      console.log(`fetching page ${pageIdx++}`)
 
-      if (!artist || !album || !url) {
-        console.error('could not pull data for line', firstLine)
-        continue
+      const items = page.locator(
+        '.search-results-list .product-listing-item .product-listing-item__info--title > a'
+      )
+      const itemsCount = await items.count()
+
+      for (let i = 0; i < itemsCount; i++) {
+        const item = items.nth(i)
+        const innerText = await item.innerText()
+        const matches = innerText.match(/(.*)[’']s? (.*)/)
+        const artist = matches?.[1]
+        const album = matches?.[2]
+        const urlPath = await item.getAttribute('href')
+
+        if (!artist || !album || !urlPath) {
+          console.error('could not pull info for item', { innerText })
+          continue
+        }
+
+        if (pageIdx === 15) {
+          console.log('what the fuck is up', page.url())
+          console.log({ album, artist, urlPath })
+        }
+
+        const slug = new URL('https://www.bloomsbury.com' + urlPath)
+        slug.searchParams.set('utm_source', 'album-mode.party')
+        slug.searchParams.set('utm_term', '33-13')
+
+        await prisma.albumReviewedByPublication
+          .create({
+            data: {
+              publicationID: publication.id,
+              album,
+              artist,
+              slug: slug.toString(),
+            },
+          })
+          .catch(() => {})
       }
 
-      const slug = new URL(url)
-      slug.searchParams.set('utm_source', 'album-mode.party')
-      slug.searchParams.set('utm_term', '33-13')
-
-      await prisma.albumReviewedByPublication
-        .create({
-          data: {
-            publicationID: publication.id,
-            album,
-            artist,
-            slug: slug.toString(),
-          },
-        })
-        .catch(() => {})
-    }
+      try {
+        await page
+          .locator(
+            '#search-header .pagination:first-of-type li:not(.disabled):last-child > button'
+          )
+          .click()
+      } catch (e) {
+        shouldContinue = false
+      }
+    } while (shouldContinue)
   } finally {
     await page.close()
     await context.close()
@@ -81,7 +103,10 @@ const US = async () => {
 }
 
 const main = async () => {
-  await US()
+  await bloomsburyScraper('33-13')
+  await bloomsburyScraper('33-13-japan')
+  await bloomsburyScraper('33-13-brazil')
+  await bloomsburyScraper('33-13-europe')
 }
 
 main()
