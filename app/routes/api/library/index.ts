565cd3f540dc0e29ae5215cd3a9e54000715adae
Eli Gundry <eligundry@gmail.com>
2023-01-05 20:58:00 -0500

    Make library routes use the new authentication pattern

--- app/routes/api/library/index.ts
+++ app/routes/api/library/index.ts
@@ -1,21 +1,20 @@
 import { ActionArgs, LoaderArgs, json } from '@remix-run/node'
 
+import { spotifyStrategy } from '~/lib/auth.server'
 import librarySync from '~/lib/librarySync.server'
-import spotifyLib from '~/lib/spotify.server'
 
 // Save an item by POSTing it to this endpoint
-export async function action({ request }: ActionArgs) {
-  const spotify = await spotifyLib.initializeFromRequest(request)
+export async function action({ request, context }: ActionArgs) {
+  const { serverTiming } = context
+  const session = await serverTiming.track('spotify.session', () =>
+    spotifyStrategy.getSession(request)
+  )
 
-  try {
-    var user = await spotify.getUser()
-
-    if (!user) {
-      throw 'nope!'
-    }
-  } catch (e) {
+  if (!session || !session.user) {
     throw json({ error: 'must be logged into spotify to use this route' }, 401)
   }
+
+  const userID = session.user.id
 
   try {
     var item = await request.json()
@@ -24,32 +23,47 @@ export async function action({ request }: ActionArgs) {
   }
 
   try {
-    await librarySync.saveItem(user.uri, item)
+    await serverTiming.track('librarySync.saveItem', () =>
+      librarySync.saveItem(userID, item)
+    )
   } catch (e: any) {
     throw json({ error: 'could not save item', detail: e?.message }, 500)
   }
 
-  return json({ msg: 'saved item', item }, 201)
+  return json(
+    { msg: 'saved item', item },
+    {
+      status: 201,
+      headers: {
+        [serverTiming.headerKey]: serverTiming.toString(),
+      },
+    }
+  )
 }
 
-export async function loader({ request }: LoaderArgs) {
-  const spotify = await spotifyLib.initializeFromRequest(request)
+export async function loader({ request, context }: LoaderArgs) {
+  const { serverTiming } = context
+  const session = await serverTiming.track('spotify.session', () =>
+    spotifyStrategy.getSession(request)
+  )
 
-  try {
-    var user = await spotify.getUser()
-
-    if (!user) {
-      throw 'nope!'
-    }
-  } catch (e) {
+  if (!session || !session.user) {
     throw json({ error: 'must be logged into spotify to use this route' }, 401)
   }
 
+  const userID = session.user.id
+
   try {
-    var library = await librarySync.getLibrary(user.uri)
+    var library = await serverTiming.track('librarySync.saveItem', () =>
+      librarySync.getLibrary(userID)
+    )
   } catch (e: any) {
     throw json({ error: 'could not fetch library', detail: e?.message }, 500)
   }
 
-  return json(library)
+  return json(library, {
+    headers: {
+      [serverTiming.headerKey]: serverTiming.toString(),
+    },
+  })
 }
