4dd18ffee02129e919eb2b8a36692dd1b4b9caf2
Eli Gundry <eligundry@gmail.com>
2023-01-13 02:30:23 -0500

    Tweak some caching values

--- app/config.ts
+++ app/config.ts
@@ -2,8 +2,9 @@ const config = {
   siteTitle: 'Album Mode.party 🎉',
   siteDescription: "Don't know what to listen to? Let us recommend an album!",
   cacheControl: {
-    public: `public, max-age=${60 * 60 * 24}, s-maxage=${60 * 60 * 24}`,
-    private: `private, max-age=${60 * 60 * 12}`,
+    public: `public, max-age=${60 * 60}, s-maxage=${60 * 60}`,
+    publicLonger: `public, max-age=${60 * 60 * 24}, s-maxage=${60 * 60 * 24}`,
+    private: `private, max-age=${60 * 60}`,
   },
   asyncRetryConfig: {
     retries: 5,
