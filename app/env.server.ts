602e7fc8b9f4871d6f0d67d75ed22fbd843c48ba
Eli Gundry <eligundry@gmail.com>
2023-01-23 21:11:54 -0500

    Setup vitest & add unit tests for the env schema

--- app/env.server.ts
+++ app/env.server.ts
@@ -1,55 +1,10 @@
-import { z } from 'zod'
+import { envSchema, webAppEnvSchema } from './lib/envSchema.server'
 
 const isNotWebApp =
   process.env.GITHUB_ACTIONS === 'true' || process.env.SEED_SCRIPT === 'true'
 
-export const envSchema = z.object({
-  APP_AWS_ACCESS_KEY_ID: z.string().default(''),
-  APP_AWS_SECRET_ACCESS_KEY: z.string().default(''),
-  AUTH_SECRETS: z.preprocess(
-    (val) => (typeof val === 'string' ? JSON.parse(val) : ['']),
-    z.array(z.string())
-  ),
-  BASIC_AUTH_USERNAME: z.string().optional(),
-  BASIC_AUTH_PASSWORD: z.string().optional(),
-  CI: z.coerce.boolean().default(false),
-  COMMIT_REF: z.string().optional(),
-  DATABASE_URL: z.string(),
-  LOGGER_EMAIL_SETTINGS: z
-    .preprocess(
-      (val) => (typeof val === 'string' ? JSON.parse(val) : null),
-      z.object({
-        publicKey: z.string(),
-        privateKey: z.string(),
-        templateID: z.string(),
-        serviceID: z.string(),
-      })
-    )
-    .optional(),
-  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-  OG_API_URL: z.string().url().default('http://localhost:3001'),
-  SEED_SCRIPT: z.coerce.boolean().default(false),
-  SENTRY_DSN: z.string().optional(),
-  SPOTIFY_CLIENT_ID: z.string(),
-  SPOTIFY_CLIENT_SECRET: z.string(),
-  TWITTER_APP_KEY: z.string().optional(),
-  TWITTER_APP_SECRET: z.string().optional(),
-  TWITTER_ACCESS_TOKEN: z.string().optional(),
-  TWITTER_ACCESS_SECRET: z.string().optional(),
-})
-
 const env = isNotWebApp
   ? envSchema.parse(process.env)
-  : envSchema
-      .extend({
-        APP_AWS_ACCESS_KEY_ID: z.string(),
-        APP_AWS_SECRET_ACCESS_KEY: z.string(),
-        AUTH_SECRETS: z.preprocess(
-          (val) => (typeof val === 'string' && val ? JSON.parse(val) : null),
-          z.array(z.string().min(2))
-        ),
-        OG_API_URL: z.string().url(),
-      })
-      .parse(process.env)
+  : webAppEnvSchema.parse(process.env)
 
 export default env
