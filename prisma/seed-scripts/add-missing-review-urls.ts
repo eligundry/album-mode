8e73db2f7d0593467d8c538d321a2343dda86ab3
Eli Gundry <eligundry@gmail.com>
2023-02-08 21:01:58 -0500

    Add ra.co review URLs (#57)
    * Add script that searches Google for missing reviews
    
    * Add escape hatch to skip albums that are unresolvable
    
    * Copy update
    
    * Update review updater to use playwright for google searches
    
    * Convert searching to DuckDuckGo and finish scraping

--- prisma/seed-scripts/add-missing-review-urls.ts
+++ prisma/seed-scripts/add-missing-review-urls.ts
@@ -1,69 +1,11 @@
 import { AlbumReviewedByPublication } from '@prisma/client'
-// @ts-ignore
-import googleIt from 'google-it'
 import inquirer from 'inquirer'
+import { chromium } from 'playwright'
 
 import { prisma } from '~/lib/db.server'
 
-const googleUpdateAlbum = async (
-  album: AlbumReviewedByPublication,
-  hostname: string
-) => {
-  const searchResults = await googleIt({
-    query: `${album.album} ${album.artist}`,
-    includeSites: `https://${hostname}`,
-    disableConsole: true,
-    userAgent:
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-  })
-
-  const { link } = await inquirer.prompt([
-    {
-      type: 'rawlist',
-      name: 'link',
-      message: `Which link for ${album.album} by ${album.artist}`,
-      choices: [
-        ...searchResults.map((res: any) => ({
-          name: `[${res.title}](${res.link})`,
-          value: res.link,
-        })),
-        {
-          name: 'Skip',
-          value: '',
-        },
-      ],
-    },
-  ])
-
-  if (!link) {
-    await prisma.albumReviewedByPublication.update({
-      data: {
-        reviewUnresolvabe: true,
-      },
-      where: {
-        id: album.id,
-      },
-    })
-
-    return
-  }
-
-  await prisma.albumReviewedByPublication
-    .update({
-      data: {
-        slug: link,
-      },
-      where: {
-        id: album.id,
-      },
-    })
-    .catch((error) => {
-      console.error(error)
-      return googleUpdateAlbum(album, hostname)
-    })
-}
-
 const main = async () => {
+  const browser = await chromium.launch()
   const publications = await prisma.publication.findMany({
     select: {
       id: true,
@@ -84,7 +26,7 @@ const main = async () => {
     {
       type: 'input',
       name: 'hostname',
-      message: 'What hostname should we search Google for?',
+      message: 'What hostname should we search DuckDuckGo for?',
     },
   ])
 
@@ -104,8 +46,134 @@ const main = async () => {
     },
   })
 
+  const duckDuckGoUpdateAlbum = async (
+    album: AlbumReviewedByPublication,
+    hostname: string
+  ) => {
+    const context = await browser.newContext({
+      userAgent:
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
+    })
+    const page = await context.newPage()
+    const url = new URL('https://duckduckgo.com/')
+    url.searchParams.set('q', `${album.album} ${album.artist} site:${hostname}`)
+    await page.goto(url.toString())
+    const linksLocator = page.locator(
+      `#links h2 a[href*="https://${hostname}"]`
+    )
+
+    try {
+      await linksLocator.first().waitFor({
+        timeout: 5 * 1000,
+      })
+    } catch (e) {
+      console.error(e)
+      console.log(page.url())
+      const { action } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'action',
+          message: `Could not search DuckDuckGo for ${album.album} by ${album.artist}. Do you want to enter URL manually?`,
+        },
+      ])
+
+      let data: Partial<AlbumReviewedByPublication> = {
+        reviewUnresolvabe: true,
+      }
+
+      if (action?.startsWith('https://')) {
+        data = {
+          slug: action as string,
+        }
+      }
+
+      await prisma.albumReviewedByPublication.update({
+        data,
+        where: {
+          id: album.id,
+        },
+      })
+
+      return
+    }
+
+    const linkElms = await linksLocator.all()
+    const options: { title: string; link: string }[] = []
+
+    for (let el of linkElms) {
+      const link = await el.getAttribute('href')
+      const title = await el.textContent()
+
+      if (!link || !title) {
+        continue
+      }
+
+      options.push({ title, link })
+    }
+
+    const { link } = await inquirer.prompt([
+      {
+        type: 'rawlist',
+        name: 'link',
+        message: `Which link for ${album.album} by ${album.artist}`,
+        choices: [
+          ...options.map((res) => ({
+            name: `[${res.title}](${res.link})`,
+            value: res.link,
+          })),
+          {
+            name: 'Skip',
+            value: '',
+          },
+        ],
+      },
+    ])
+
+    if (!link) {
+      await prisma.albumReviewedByPublication.update({
+        data: {
+          reviewUnresolvabe: true,
+        },
+        where: {
+          id: album.id,
+        },
+      })
+
+      return
+    }
+
+    await prisma.albumReviewedByPublication
+      .update({
+        data: {
+          slug: link,
+        },
+        where: {
+          id: album.id,
+        },
+      })
+      .catch(async (error) => {
+        if (error.message.includes('slug')) {
+          await prisma.albumReviewedByPublication.update({
+            data: {
+              slug: link + `#${album.id}`,
+            },
+            where: {
+              id: album.id,
+            },
+          })
+
+          return
+        }
+
+        console.error(error)
+        return duckDuckGoUpdateAlbum(album, hostname)
+      })
+
+    await context.close()
+  }
+
   for (let album of reviews) {
-    await googleUpdateAlbum(album, hostname)
+    await duckDuckGoUpdateAlbum(album, hostname)
   }
 }
 
