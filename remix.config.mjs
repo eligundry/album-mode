8f5bc57644626165c2e51377aad4884460fbb456
Eli Gundry <eligundry@gmail.com>
2023-06-24 14:10:01 -0400

    Update canonical url to be set in `meta` function
    Update server build target stuff in advance of v2

--- remix.config.mjs
+++ remix.config.mjs
@@ -7,14 +7,22 @@ export default {
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? './server.ts'
       : undefined,
+
+  publicPath: '/build/',
   serverBuildPath: '.netlify/functions-internal/server.js',
-  serverDependenciesToBundle: ['@eligundry/server-timing'],
+  serverConditions: 'deno, worker',
+  serverMainFields: 'main, module',
   serverModuleFormat: 'cjs',
+  serverPlatform: 'node',
+  serverMinify: false,
+
+  serverDependenciesToBundle: ['@eligundry/server-timing'],
   tailwind: true,
   postcss: true,
   future: {
     v2_errorBoundary: true,
     v2_headers: true,
     v2_meta: true,
+    v2_normalizeFormMethod: true,
   },
 }
