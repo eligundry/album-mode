19f5c380b6891b35923cef226e7038c9bfde6cb9
Eli Gundry <eligundry@gmail.com>
2023-08-24 03:09:30 -0400

    Add 1 hour of caching to top artists

--- app/routes/api.user-artists.ts
+++ app/routes/api.user-artists.ts
@@ -2,9 +2,17 @@ import { LoaderFunction, json } from '@remix-run/node'
 
 import spotifyLib from '~/lib/spotify.server'
 
+import config from '~/config'
+
 export const loader: LoaderFunction = async ({ request, context }) => {
+  const { serverTiming } = context
   const spotify = await spotifyLib.initializeFromRequest(request, context)
   const artists = await spotify.getUserTopArtists()
 
-  return json(artists)
+  return json(artists, {
+    headers: {
+      'cache-control': config.cacheControl.private,
+      [serverTiming.headerKey]: serverTiming.toString(),
+    },
+  })
 }
